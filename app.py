fec28e0086128007ca720beec50fe7bcdb911d0f
khi <hyungin414@naver.com>
2023-06-08 22:50:06 +0900

    rm cache

--- app.py
+++ app.py
@@ -295,4 +295,4 @@ def get_future_price():
 
     
 if __name__ == '__main__':
-    app.run(debug=True) #host="0.0.0.0", port="8080",
+    app.run(host="0.0.0.0", port="8080",debug=True) #host="0.0.0.0", port="8080",
